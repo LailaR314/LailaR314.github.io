bbdb1595671e47fccbbd4494c40df7b9b81d3e74
LailaR314 <ar0288a@american.edu>
2017-01-29 14:00:01 -0500

    Add files via upload

---
+++ ARscript.js
@@ -0,0 +1,23 @@
+ var myName = "Anisa";
+ function sayHello(myName){
+     alert("Hello " + myName)
+    }
+
+var animals = document.getElementsByClassName("animal")
+console.log("Hello Anisa")
+
+function getanimalmammal(){
+  // get the user's favorite animal
+  var fave = prompt("What is your favorite mammal?")
+  console.log(fave)
+  // store the favorite mammal as a variable
+  // change the best mammal to match the user's input
+}
+//if the value of fave is empty
+if (fave != ''){
+
+//if user does enter animal
+} else {
+
+}
+getanimalmammal()
